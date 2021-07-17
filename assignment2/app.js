3d678504247126ff419c4fa98547658350b28783
Sumit Upadhyay <sumit.upadhyay@vmlyr.com>
2021-07-17 17:45:13 +0530

    middleware routing

--- assignment2/app.js
+++ assignment2/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('Middleware One');
-    next();
+// app.use((req,res,next) => {
+//     console.log('Middleware One');
+//     next();
+// });
+
+// app.use((req,res,next) => {
+//      console.log('Middleware Two');
+//     res.send('express js middleware');
+// });
+app.use('/user',(req,res,next) => {
+    console.info('user page middleware');
+    res.send('<p>This middleware handles userpage /user </p>')
 });
 
-app.use((req,res,next) => {
-     console.log('Middleware Two');
-    res.send('express js middleware');
+app.use('/',(req,res,next) => {
+    console.warn('home page middleware');
+    res.send('<div>This middleware handles homepage /</div>')
 });
+
+
 
 app.listen(4000);
